18fd68d9a82ed0b461f0ed72bc30b6420178aa99
Nagarjuna-Gutta <nagaturservice@gmail.com>
2016-08-12 07:37:52 -0700

    Add files via upload

---
+++ learn_chef_httpd/learn_chef_httpd/recipes/default.rb
@@ -0,0 +1,68 @@
+#
+# Cookbook Name:: learn_chef_httpd
+# Recipe:: default
+
+#
+# Copyright (C) 2014
+#
+#
+
+
+package 'httpd'
+
+
+yum_package 'mod_ssl'
+
+yum_package 'openssl'
+
+
+directory '/etc/ssl/certs' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  
+action :create
+
+end
+
+
+template '/etc/ssl/certs/test.crt' do
+ 
+ source 'test.crt.erb'
+
+end
+
+
+template '/etc/ssl/certs/test.key' do
+ 
+ source 'test.key.erb'
+
+end
+
+
+template '/var/www/html/index.html' do
+
+  source 'index.html.erb'
+
+end
+
+template '/etc/httpd/conf/httpd.conf' do
+ 
+ source 'httpd.conf.erb'
+
+end
+
+
+
+service 'iptables' do
+  
+action :stop
+
+end
+
+
+service 'httpd' do
+  
+action [:enable, :start]
+
+end
